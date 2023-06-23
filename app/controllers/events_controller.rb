60742e59fea8b1a502ea61da4ee77d6e277fe4db
zoink506 <benhubble03@gmail.com>
2023-06-23 15:39:16 +1000

    Create before_action to limit event creation page to logged in users only

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,6 @@
 class EventsController < ApplicationController
+  before_action :logged_in?, except: [:index, :show]
+
   def index
     @events = Event.all.includes(:creator)
   end
@@ -12,5 +14,25 @@ class EventsController < ApplicationController
   end
 
   def create
+    # add before_action to limit :new, :create to only logged in users
+    @event = current_user.events.build(event_parameters)
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :new, :status => :unprocessable_entity
+    end
   end
+
+  private
+
+    def event_parameters
+      params.require(:event).permit(:title, :body, :location, :date)
+    end
+
+    def logged_in?
+      if !current_user.present?
+        flash[:alert] = "You must be logged in to view this page"
+        redirect_to root_path
+      end
+    end
 end

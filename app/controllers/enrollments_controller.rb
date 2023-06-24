fa091abd93f60eef9a6d9f2ef3d6b5aea1340f03
zoink506 <benhubble03@gmail.com>
2023-06-24 13:57:48 +1000

    Complete EnrollmentsController#create to save the new enrollment to database

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,8 +2,16 @@ class EnrollmentsController < ApplicationController
   before_action :require_login
 
   def create
-    p "current user id: #{current_user.id}"
-    p "current event: #{params[:event_id]}"
+    @enrollment = Enrollment.new(user_id: current_user.id, event_id: params[:event_id])
+    p @enrollment
+
+    if @enrollment.save
+      flash[:notice] = "Congratulations, you are now attending this event!"
+    else
+      flash[:alert] = "Sorry! Something went wrong while finalizing your attendance, please try again later"
+    end
+
+    redirect_to event_path(params[:event_id])
   end
 
   private
